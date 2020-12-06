2f9438d3ecd8286bcb57b89eb55f434c85ad62e9
Jason Wu <jason.wu2153@gmail.com>
2020-12-06 09:27:50 -0500

    moved helper functions to helpers

---
+++ helpers.py
@@ -0,0 +1,14 @@
+sec_url = 'https://www.sec.gov'
+
+def get_request(url):
+'Returns result of a http request to url.'
+    return requests.get(url)
+
+def create_url(cik):
+    'Returns url of SEC 13F filings given a CIK.'
+    return f'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={cik}&owner=exclude&count=40'
+
+def get_user_input():
+    'Prompts user to enter a CIK number.'
+    cik = input('Enter 10-digit CIK number: ')
+    return cik
